02376fe5074b0ff19cec46c94848799947be95e1
Ulf Hansson <ulf.hansson@linaro.org>
2018-01-02 17:08:50 +0100

    PM / core: Assign the wakeup_path status flag in __device_prepare()
    The PM core in the device_prepare() phase, resets the wakeup_path status
    flag to the value of device_may_wakeup(). This means if a ->prepare() or a
    ->suspend() callback for the device would update the device's wakeup
    setting, this doesn't become reflected in the wakeup_path status flag.
    
    In general this isn't a problem, because wakeup settings are not supposed
    to be changed (via for example calling device_set_wakeup_enable()) during
    any system wide suspend/resume phase.  Nevertheless there are some users,
    which can be considered as legacy, that don't conform to this behaviour.
    
    These legacy cases should be corrected, however until that is done, let's
    address the issue from the PM core, by moving the assignment of the
    wakeup_path status flag to the __device_suspend() phase and after the
    ->suspend() callback has been invoked.
    
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -1567,6 +1567,9 @@ static int __device_suspend(struct device *dev, pm_message_t state, bool async)
  End:
 	if (!error) {
 		dev->power.is_suspended = true;
+		if (device_may_wakeup(dev))
+			dev->power.wakeup_path = true;
+
 		dpm_propagate_to_parent(dev);
 		dpm_clear_suppliers_direct_complete(dev);
 	}
@@ -1687,7 +1690,7 @@ static int device_prepare(struct device *dev, pm_message_t state)
 
 	device_lock(dev);
 
-	dev->power.wakeup_path = device_may_wakeup(dev);
+	dev->power.wakeup_path = false;
 
 	if (dev->power.no_pm_callbacks) {
 		ret = 1;	/* Let device go direct_complete */
