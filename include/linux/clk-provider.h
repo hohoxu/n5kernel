be992f99bf63221ad3c7c924424054c16e09f1ee
Jerome Brunet <jbrunet@baylibre.com>
2018-02-14 14:43:39 +0100

    clk: divider: read-only divider can propagate rate change
    When a divider clock has CLK_DIVIDER_READ_ONLY set, it means that the
    register shall be left un-touched, but it does not mean the clock
    should stop rate propagation if CLK_SET_RATE_PARENT is set
    
    This is properly handled in qcom clk-regmap-divider but it was not in
    the generic divider
    
    To fix this situation, introduce a new helper function
    divider_ro_round_rate, on the same model as divider_round_rate.
    
    Fixes: e6d5e7d90be9 ("clk-divider: Fix READ_ONLY when divider > 1")
    Signed-off-by: Jerome Brunet <jbrunet@baylibre.com>
    Tested-By: David Lechner <david@lechnology.com>
    Signed-off-by: Michael Turquette <mturquette@baylibre.com>
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>
    Signed-off-by: HouHongxun <houhongxun@gmail.com>

--- include/linux/clk-provider.h
+++ include/linux/clk-provider.h
@@ -508,6 +508,10 @@ unsigned long divider_recalc_rate(struct clk_hw *hw, unsigned long parent_rate,
 long divider_round_rate(struct clk_hw *hw, unsigned long rate,
 		unsigned long *prate, const struct clk_div_table *table,
 		u8 width, unsigned long flags);
+long divider_ro_round_rate_parent(struct clk_hw *hw, struct clk_hw *parent,
+				  unsigned long rate, unsigned long *prate,
+				  const struct clk_div_table *table, u8 width,
+				  unsigned long flags, unsigned int val);
 int divider_get_val(unsigned long rate, unsigned long parent_rate,
 		const struct clk_div_table *table, u8 width,
 		unsigned long flags);
@@ -851,6 +855,17 @@ static inline void __clk_hw_set_clk(struct clk_hw *dst, struct clk_hw *src)
 {
 	dst->clk = src->clk;
 	dst->core = src->core;
+}
+
+static inline long divider_ro_round_rate(struct clk_hw *hw, unsigned long rate,
+					 unsigned long *prate,
+					 const struct clk_div_table *table,
+					 u8 width, unsigned long flags,
+					 unsigned int val)
+{
+	return divider_ro_round_rate_parent(hw, clk_hw_get_parent(hw),
+					    rate, prate, table, width, flags,
+					    val);
 }
 
 /*
