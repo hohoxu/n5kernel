beff7f33976e7e633f4c07a446c0091cdde4030f
Eric Dumazet <edumazet@google.com>
2018-05-17 14:47:28 -0700

    tcp: add tcp_comp_sack_delay_ns sysctl
    This per netns sysctl allows for TCP SACK compression fine-tuning.
    
    Its default value is 1,000,000, or 1 ms to meet TSO autosizing period.
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Acked-by: Neal Cardwell <ncardwell@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: HouHongxun <houhongxun@gmail.com>

--- net/ipv4/tcp_ipv4.c
+++ net/ipv4/tcp_ipv4.c
@@ -2517,6 +2517,7 @@ static int __net_init tcp_sk_init(struct net *net)
 	net->ipv4.sysctl_tcp_orphan_retries = 0;
 	net->ipv4.sysctl_tcp_fin_timeout = TCP_FIN_TIMEOUT;
 	net->ipv4.sysctl_tcp_notsent_lowat = UINT_MAX;
+	net->ipv4.sysctl_tcp_comp_sack_delay_ns = NSEC_PER_MSEC;
 
 	return 0;
 fail:
