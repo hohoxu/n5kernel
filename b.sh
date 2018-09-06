828c0ad43e5200bcf494c51cf7b493f42ede52bb
HouHongxun <houhongxun@gmail.com>
2018-09-06 11:25:28 +0800

    Add build script

---
+++ b.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+export PATH=/home/hongxun/build/aarch64-linux-android-4.9/bin:/study/aarch64-linux-android-4.9/bin:$PATH
+
+outdir=../out-oneplus
+
+if [ ! -d "${outdir}" ]
+then
+	rm -rf "${outdir}"
+	make O=${outdir} ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- sdm845-perf_defconfig
+fi
+
+make O=${outdir} ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- -j4
+
+cd ${outdir}
+
+cp -v arch/arm64/boot/Image.gz ../bootdir/kernel/
+cp -v $(find . -name \*\.dtb) ../bootdir/dtbs/
+cd ../bootdir
+rm -f ../kernel.zip
+zip -r ../kernel.zip *
