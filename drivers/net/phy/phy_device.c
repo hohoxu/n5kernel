e49fe35e284edb45b41315b2a40d1e4eae89dad0
Heiner Kallweit <hkallweit1@gmail.com>
2018-09-24 22:01:32 +0200

    net: phy: fix WoL handling when suspending the PHY
    Core of the problem is that phy_suspend() suspends the PHY when it
    should not because of WoL. phy_suspend() checks for WoL already, but
    this works only if the PHY driver handles WoL (what is rarely the case).
    Typically WoL is handled by the MAC driver.
    
    This patch uses new member wol_enabled of struct net_device as
    additional criteria in the check when not to suspend the PHY because
    of WoL.
    
    Last but not least change phy_detach() to call phy_suspend() before
    attached_dev is set to NULL. phy_suspend() accesses attached_dev
    when checking whether the MAC driver activated WoL.
    
    Fixes: f1e911d5d0df ("r8169: add basic phylib support")
    Fixes: e8cfd9d6c772 ("net: phy: call state machine synchronously in phy_stop")
    Signed-off-by: Heiner Kallweit <hkallweit1@gmail.com>
    Reviewed-by: Florian Fainelli <f.fainelli@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: HouHongxun <houhongxun@gmail.com>

--- drivers/net/phy/phy_device.c
+++ drivers/net/phy/phy_device.c
@@ -97,7 +97,12 @@ static bool mdio_bus_phy_may_suspend(struct phy_device *phydev)
 	if (!netdev)
 		return !phydev->suspended;
 
-	/* Don't suspend PHY if the attached netdev parent may wakeup.
+	if (netdev->wol_enabled)
+		return false;
+
+	/* As long as not all affected network drivers support the
+	 * wol_enabled flag, let's check for hints that WoL is enabled.
+	 * Don't suspend PHY if the attached netdev parent may wake up.
 	 * The parent may point to a PCI device, as in tg3 driver.
 	 */
 	if (netdev->dev.parent && device_may_wakeup(netdev->dev.parent))
@@ -1008,9 +1013,9 @@ void phy_detach(struct phy_device *phydev)
 	struct mii_bus *bus;
 	int i;
 
+	phy_suspend(phydev);
 	phydev->attached_dev->phydev = NULL;
 	phydev->attached_dev = NULL;
-	phy_suspend(phydev);
 
 	module_put(phydev->mdio.dev.driver->owner);
 
@@ -1042,12 +1047,13 @@ EXPORT_SYMBOL(phy_detach);
 int phy_suspend(struct phy_device *phydev)
 {
 	struct phy_driver *phydrv = to_phy_driver(phydev->mdio.dev.driver);
+	struct net_device *netdev = phydev->attached_dev;
 	struct ethtool_wolinfo wol = { .cmd = ETHTOOL_GWOL };
 	int ret = 0;
 
 	/* If the device has WOL enabled, we cannot suspend the PHY */
 	phy_ethtool_get_wol(phydev, &wol);
-	if (wol.wolopts)
+	if (wol.wolopts || (netdev && netdev->wol_enabled))
 		return -EBUSY;
 
 	if (phydrv->suspend)
