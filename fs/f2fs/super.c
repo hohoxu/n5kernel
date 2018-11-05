350d0290fbf11b5f1826ec6f6344fe7fc0f6663b
HouHongxun <houhongxun@gmail.com>
2018-11-05 16:44:24 +0800

    Revert "f2fs: quota: fix incorrect comments"
    This reverts commit 137a736c4c18096f11688f357901018c095652fc.

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -2960,7 +2960,10 @@ try_onemore:
 		goto free_root_inode;
 
 #ifdef CONFIG_QUOTA
-	/* Enable quota usage during mount */
+	/*
+	 * Turn on quotas which were not enabled for read-only mounts if
+	 * filesystem has quota feature, so that they are updated correctly.
+	 */
 	if (f2fs_sb_has_quota_ino(sb) && !f2fs_readonly(sb)) {
 		err = f2fs_enable_quotas(sb);
 		if (err) {
