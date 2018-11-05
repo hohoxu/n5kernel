ef1f05f2f25bdea0e845395bd40964e3abba54b0
HouHongxun <houhongxun@gmail.com>
2018-11-05 16:44:24 +0800

    Revert "f2fs: fix return value in f2fs_ioc_commit_atomic_write"
    This reverts commit a0fbb1c7dfe9b8a81063e5de9463ad8eaa0f18d7.

--- fs/f2fs/file.c
+++ fs/f2fs/file.c
@@ -1721,10 +1721,8 @@ static int f2fs_ioc_commit_atomic_write(struct file *filp)
 
 	down_write(&F2FS_I(inode)->dio_rwsem[WRITE]);
 
-	if (f2fs_is_volatile_file(inode)) {
-		ret = -EINVAL;
+	if (f2fs_is_volatile_file(inode))
 		goto err_out;
-	}
 
 	if (f2fs_is_atomic_file(inode)) {
 		ret = commit_inmem_pages(inode);
