67bb38b69ff773b19679d1f3257ddcaacf518e0e
HouHongxun <houhongxun@gmail.com>
2018-11-05 16:44:24 +0800

    Revert "f2fs: rework fault injection handling to avoid a warning"
    This reverts commit 40a547065a4b4b97499237781ec119c1a17c0023.

--- fs/f2fs/recovery.c
+++ fs/f2fs/recovery.c
@@ -518,9 +518,10 @@ retry_dn:
 
 			if (src == NULL_ADDR) {
 				err = f2fs_reserve_new_block(&dn);
-				while (err &&
-				       IS_ENABLED(CONFIG_F2FS_FAULT_INJECTION))
+#ifdef CONFIG_F2FS_FAULT_INJECTION
+				while (err)
 					err = f2fs_reserve_new_block(&dn);
+#endif
 				/* We should not get -ENOSPC */
 				f2fs_bug_on(sbi, err);
 				if (err)
