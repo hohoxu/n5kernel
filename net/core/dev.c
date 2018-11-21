cb72e8816a004fa8a0165a98f35b00b1727dab25
Paolo Abeni <pabeni@redhat.com>
2018-11-21 18:21:35 +0100

    net: don't keep lonely packets forever in the gro hash
    Eric noted that with UDP GRO and NAPI timeout, we could keep a single
    UDP packet inside the GRO hash forever, if the related NAPI instance
    calls napi_gro_complete() at an higher frequency than the NAPI timeout.
    Willem noted that even TCP packets could be trapped there, till the
    next retransmission.
    This patch tries to address the issue, flushing the old packets -
    those with a NAPI_GRO_CB age before the current jiffy - before scheduling
    the NAPI timeout. The rationale is that such a timeout should be
    well below a jiffy and we are not flushing packets eligible for sane GRO.
    
    v1  -> v2:
     - clarified the commit message and comment
    
    RFC -> v1:
     - added 'Fixes tags', cleaned-up the wording.
    
    Reported-by: Eric Dumazet <eric.dumazet@gmail.com>
    Fixes: 3b47d30396ba ("net: gro: add a per device gro flush timer")
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Acked-by: Willem de Bruijn <willemb@google.com>
    Acked-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/dev.c
+++ net/core/dev.c
@@ -5157,11 +5157,14 @@ void napi_complete_done(struct napi_struct *n, int work_done)
 		if (work_done)
 			timeout = n->dev->gro_flush_timeout;
 
+		/* When the NAPI instance uses a timeout and keeps postponing
+		 * it, we need to bound somehow the time packets are kept in
+		 * the GRO layer
+		 */
+		napi_gro_flush(n, !!timeout);
 		if (timeout)
 			hrtimer_start(&n->timer, ns_to_ktime(timeout),
 				      HRTIMER_MODE_REL_PINNED);
-		else
-			napi_gro_flush(n, false);
 	}
 	if (likely(list_empty(&n->poll_list))) {
 		WARN_ON_ONCE(!test_and_clear_bit(NAPI_STATE_SCHED, &n->state));
