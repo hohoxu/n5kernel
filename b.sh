f47ff664b104b50751e3933312414d15309b4eab
HouHongxun <houhongxun@gmail.com>
2018-12-12 16:13:16 +0800

    Use linaro toolchain

--- b.sh
+++ b.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-export PATH=/misc/aarch64-linux-android-4.9/bin:/study/aarch64-linux-android-4.9/bin:$PATH
+export PATH=/misc/gcc-arm-8.2-2018.11-x86_64-aarch64-linux-gnu/bin:/study/gcc-arm-8.2-2018.11-x86_64-aarch64-linux-gnu/bin:$PATH
 
 outdir=../out-oneplus
 
 if [ ! -d "${outdir}" ]
 then
 	rm -rf "${outdir}"
-	make O=${outdir} ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- sdm845-perf_defconfig
+	make O=${outdir} ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- sdm845-perf_defconfig
 fi
 
-make O=${outdir} ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- -j4
+make O=${outdir} ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- -j4
 
 cd ${outdir}
 
