663ff106147733694bc8d809ca18938c63b87cfa
Tetsuo Handa <penguin-kernel@I-love.SAKURA.ne.jp>
2019-04-12 19:53:38 +0900

    net: netlink: Check address length before reading groups field
    KMSAN will complain if valid address length passed to bind() is shorter
    than sizeof(struct sockaddr_nl) bytes.
    
    Signed-off-by: Tetsuo Handa <penguin-kernel@I-love.SAKURA.ne.jp>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: HouHongxun <houhongxun@gmail.com>

--- net/netlink/af_netlink.c
+++ net/netlink/af_netlink.c
@@ -968,7 +968,7 @@ static int netlink_bind(struct socket *sock, struct sockaddr *addr,
 	struct netlink_sock *nlk = nlk_sk(sk);
 	struct sockaddr_nl *nladdr = (struct sockaddr_nl *)addr;
 	int err;
-	long unsigned int groups = nladdr->nl_groups;
+	unsigned long groups;
 	bool bound;
 
 	if (addr_len < sizeof(struct sockaddr_nl))
@@ -976,6 +976,7 @@ static int netlink_bind(struct socket *sock, struct sockaddr *addr,
 
 	if (nladdr->nl_family != AF_NETLINK)
 		return -EINVAL;
+	groups = nladdr->nl_groups;
 
 	/* Only superuser is allowed to listen multicasts */
 	if (groups) {
