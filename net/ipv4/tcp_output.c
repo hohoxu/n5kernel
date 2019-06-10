2b235b8400f7c00b4e1731581a554edfcc85cdda
HouHongxun <houhongxun@gmail.com>
2019-06-10 14:18:07 +0800

    Revert "tcp: when scheduling TLP, time of RTO should account for current ACK"
    This reverts commit c1dc149c7ab0f7ec7195f16adf70d25a187a2c18.

--- net/ipv4/tcp_output.c
+++ net/ipv4/tcp_output.c
@@ -2225,7 +2225,7 @@ repair:
 
 		/* Send one loss probe per tail loss episode. */
 		if (push_one != 2)
-			tcp_schedule_loss_probe(sk, false);
+			tcp_schedule_loss_probe(sk);
 		is_cwnd_limited |= (tcp_packets_in_flight(tp) >= tp->snd_cwnd);
 		tcp_cwnd_validate(sk, is_cwnd_limited);
 		return false;
@@ -2233,7 +2233,7 @@ repair:
 	return !tp->packets_out && tcp_send_head(sk);
 }
 
-bool tcp_schedule_loss_probe(struct sock *sk, bool advancing_rto)
+bool tcp_schedule_loss_probe(struct sock *sk)
 {
 	struct tcp_sock *tp = tcp_sk(sk);
 	u32 rtt = usecs_to_jiffies(tp->srtt_us >> 3);
@@ -2267,9 +2267,7 @@ bool tcp_schedule_loss_probe(struct sock *sk, bool advancing_rto)
 	timeout = max_t(u32, timeout, msecs_to_jiffies(10));
 
 	/* If the RTO formula yields an earlier time, then use that time. */
-	rto_delta = advancing_rto ?
-		    inet_csk(sk)->icsk_rto :
-		    tcp_rto_delta(sk);  /* How far in future is RTO? */
+	rto_delta = tcp_rto_delta(sk);  /* How far in future is RTO? */
 	if (rto_delta > 0)
 		timeout = min_t(u32, timeout, rto_delta);
 
