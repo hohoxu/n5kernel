6dce709c3070362cda95663bd7f2ba02aa8c8095
Eric Dumazet <edumazet@google.com>
2019-10-09 15:10:15 -0700

    tcp: annotate lockless access to tcp_memory_pressure
    tcp_memory_pressure is read without holding any lock,
    and its value could be changed on other cpus.
    
    Use READ_ONCE() to annotate these lockless reads.
    
    The write side is already using atomic ops.
    
    Fixes: b8da51ebb1aa ("tcp: introduce tcp_under_memory_pressure()")
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Signed-off-by: HouHongxun <houhongxun@gmail.com>

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -332,7 +332,7 @@ EXPORT_SYMBOL(tcp_memory_pressure);
 
 void tcp_enter_memory_pressure(struct sock *sk)
 {
-	if (!tcp_memory_pressure) {
+	if (!READ_ONCE(tcp_memory_pressure)) {
 		NET_INC_STATS(sock_net(sk), LINUX_MIB_TCPMEMORYPRESSURES);
 		tcp_memory_pressure = 1;
 	}
